18b4d4754d50c5af3ffc2118b85430a55147cbda
elroi773 <s2457@e-mirim.hs.kr>
2026-01-07 14:10:34 +0900

    use State hook 에 실습한 코드 입니다

--- yirea_react02/src/App.jsx
+++ yirea_react02/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react"
+import { useState } from "react";
 function App() {
   let [state, setState] = useState(0); //구조분해 할당
   const [blub, setBlub] = useState("OFF");
+  const toggleBlub = () => {
+    setBlub((prev) => (prev === "ON" ? "OFF" : "ON"));
+  };
   const btnClick = () => {
-    setState(state + 1);
-  }
+    setState((prev) => prev + 1);
+  };
   console.log(state);
-  return (
+  const Light = ({isOn}) => {
+    return(
+      <>
+        {isOn === "ON" ? (
+          <h1 style={{backgroundColor: "orange"}}>ON</h1>
+        ) : (
+          <h1 style={{backgroundColor: "gray"}}>OFF</h1>
+        )}
+      </>
+    )
+  }
+   return (
     <>
       <div>
         <h1>{state}</h1>
@@ -14,18 +28,11 @@ function App() {
       </div>
       <div>
         {/* 끄고 켜기 버튼 */}
-        <h1>{blub}</h1>
-        <button onClick={() => {
-          if (blub === "OFF") {
-            setBlub("ON");
-          } else {
-            setBlub("OFF");
-          }
-        }}>토글</button> 
+        <Light isOn={blub} />
+        <button onClick={toggleBlub}>끄기/켜기</button> 
       </div>
     </>
   )
 }
 
 export default App
-
