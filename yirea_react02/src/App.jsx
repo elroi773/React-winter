f40e55cccb7495e752242a3f33ad2667fe476cbb
elroi773 <s2457@e-mirim.hs.kr>
2026-01-07 13:44:10 +0900

    useState 에 대해 실습 했습니다

--- yirea_react02/src/App.jsx
+++ yirea_react02/src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react"
 function App() {
   let [state, setState] = useState(0); //구조분해 할당
-
+  const [blub, setBlub] = useState("OFF");
   const btnClick = () => {
     setState(state + 1);
   }
-  console.log(stateVar);
+  console.log(state);
   return (
     <>
       <div>
         <h1>{state}</h1>
         <button onClick={btnClick}>+</button>
+      </div>
+      <div>
+        {/* 끄고 켜기 버튼 */}
+        <h1>{blub}</h1>
+        <button onClick={() => {
+          if (blub === "OFF") {
+            setBlub("ON");
+          } else {
+            setBlub("OFF");
+          }
+        }}>토글</button> 
       </div>
     </>
   )
