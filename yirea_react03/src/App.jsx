42b94ea0f3f9d9ff5167e3fbd2cd3ded86929f7f
elroi773 <s2457@e-mirim.hs.kr>
2026-01-08 14:52:34 +0900

    useRef Effect 실습 예제 코드 입니다

--- yirea_react03/src/App.jsx
+++ yirea_react03/src/App.jsx
@@ -1,11 +1,38 @@
-import { useState } from 'react'
-function App() {
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
+import Even from "./components/Even";
+import { useState, useEffect } from "react";
 
+function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect 업데이트");
+  }, [count]);
+
+  function onClickButton(num) {
+    setCount(count + num);
+  }
   return (
     <>
-      
+      <h1>simple Counter</h1>
+      <input
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      {input}
+      <div>
+        <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
+      </div>
+      <div>
+        <Controller onClickButton={onClickButton} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
