67304383c5ef8754b2e5dc0f72295357263cb03a
elroi773 <s2457@e-mirim.hs.kr>
2026-01-09 22:59:18 +0900

    useNavigate 실습 예제 코드 입니다

---
+++ yirea_react03/src/pages/TimerPage.jsx
@@ -0,0 +1,11 @@
+import Timer from '../components/Timer';
+import { useState, useEffect, useRef } from "react";
+export default function TimerPage(){
+    const [isTimer, setTimer] = useState(false);
+  return(
+    <>
+      <button onClick={() => setTimer(!isTimer)}>타이머{isTimer ? '중지' : '시작'}</button>
+      {isTimer === true ? <Timer /> : null}
+    </>
+  )
+}
