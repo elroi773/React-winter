db22f016752e6294b84299a3c90bd6890fafe891
elroi773 <s2457@e-mirim.hs.kr>
2026-01-09 22:59:11 +0900

    useNavigate 실습 예제 코드 입니다

---
+++ yirea_react03/src/components/UserList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+export default function UserList() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((re) => re.json())
+      .then((data)=>{
+        setUsers(data);
+      })
+      .catch((err)=> console.error("데이터 로드 실패~ ! : ", err));
+  }, []);
+  
+  return (
+    <>
+      <h2>사용자 목록</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
