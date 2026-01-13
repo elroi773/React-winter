0342d9d07230f0a29b7fd34dbccd2e8db2be6d58
elroi773 <s2457@e-mirim.hs.kr>
2026-01-13 13:47:02 +0900

    ToDoList 실습

---
+++ yirea_react04/src/components/common/Navi.jsx
@@ -0,0 +1,21 @@
+import { Link, useNavigate } from "react-router-dom";
+function Navi() {
+  const nav = useNavigate();
+
+  function onClickButton() {
+    nav("/usermenu");
+  }
+  return (
+    <>
+      <div>
+        <Link to={"/"}>[Home]</Link>
+        <Link to={"/usermenu"}>[사용자목록]</Link>
+        <Link to={"/timermenu"}>[타이머]</Link>
+        <button onClick={onClickButton}>사용자목록 버튼</button>
+        <Link to={"/simplecountermenu"}>[간단한 카운터]</Link>
+        <Link to={"/todomenu"}>[Todo list 앱]</Link>
+      </div>
+    </>
+  );
+}
+export default Navi;
