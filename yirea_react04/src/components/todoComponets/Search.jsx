fae06da8c0e4dbadd9288e86f08d95edfb76e090
elroi773 <s2457@e-mirim.hs.kr>
2026-01-14 13:33:49 +0900

    useParm hook 함수 사용 예제 입니다

---
+++ yirea_react04/src/components/todoComponets/Search.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export default function Search({ search = "", onSearch }) {
+  const [keyword, setKeyword] = useState(search);
+
+  useEffect(() => {
+    setKeyword(search);
+  }, [search]);
+
+  useEffect(() => {
+    if (typeof onSearch === "function") onSearch(keyword);
+  }, [keyword, onSearch]);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        value={keyword}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
